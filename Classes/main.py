9ac8ed2cff0732983b745a083b691004fd2a7829
João <jv.davila@outlook>
2023-04-15 16:40:17 -0300

    print

--- Classes/main.py
+++ Classes/main.py
@@ -14,3 +14,9 @@ cliente2 = Cliente("Maria", "987.654.321-00",
 
 cliente1.comprar_ingresso(sessao1, 2)
 cliente2.comprar_ingresso(sessao1, 3)
+
+# Assuming you have a Filme object called 'filme1'
+print("Título: ", filme1.titulo)
+print("Duração: ", filme1.duracao)
+print("Gênero: ", filme1.genero)
+print("Classificação indicativa: ", filme1.classificacao)
