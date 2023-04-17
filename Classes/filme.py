cc8273b347e7c3e76900d832d1624ed124ec0f95
João Vitor D'avila <75771786+JoaoVitorDavila@users.noreply.github.com>
2023-04-17 20:43:00 -0300

    Commiting branches

--- Classes/filme.py
+++
@@ -1,6 +0,0 @@
-class Filme:
-    def __init__(self, nome: str, duracao: str, genero: str, classificacao: str):
-        self.nome = nome
-        self.duracao = duracao
-        self.genero = genero
-        self.classificacao = classificacao
